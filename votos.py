03b2b43a3ad6955ffc160257a8fd54da6216e81d
juanmamax123 <158304755+juanmamax123@users.noreply.github.com>
2024-01-31 11:38:20 -0600

    version 4

--- votos.py
+++ votos.py
@@ -4,14 +4,36 @@ if len(dpi)==13 :
     print("el candidato para presidente es partido rojo ")
     print("el candidato para presidente es partido azul ")
     presidente=input("ingrese su voto: ")
+    if presidente=="partido rojo":
+        print("vote partido rojo")
+    elif presidente== "partido azul":
+        print ("vote partido azul")
+    else:
+        print ("voto invalido")
     print(presidente)
     print("el candidato para alcalde es partido rojo")
     print("el candidato para alcalde es partido azul")
     print("el candidato para alcalde es partido verde")
     print("el candidato para alcalde es partido morado")
     alcalde=input("ingrese su voto: ")
-    print ("alcalde")
+    if alcalde=="partido azul":
+        print("vote partido azul")
+    elif alcalde== "partido rojo":
+        print ("vote partido rojo")
+    elif alcalde == "partido verde" :
+            print ("vote partido verde")
+    elif alcalde == "partido morado" :
+            print ("vote partido morado")
+    else:
+        print ("voto invalido")
+    print (alcalde)
+    guardar=input("quiere guardar su voto: si  no  ")
+    if guardar ==("si"):
+        print ("sus votos fueron", presidente , alcalde)
+    elif guardar ==("no"):
+        print("gracias por su voto")
+    else:
+         print("no respondio")
     print("se logro, nunca va a ganar la une")
 else:
     print("dpi incorrecto")
-    print("hello moto moto its incorrect")
