c7520d68eec56524954b9ca6acd37ff36705c042
juanmamax123 <158304755+juanmamax123@users.noreply.github.com>
2024-01-31 09:18:09 -0600

    versión 1

---
+++ votos.py
@@ -0,0 +1,15 @@
+dpi=(input("ingrese su dpi: "))
+if len(dpi)==13 :
+    print("dpi correcto")
+    print("el candidato para presidente es partido rojo ")
+    print("el candidato para presidente es partido azul ")
+    presidente=input("ingrese su voto: ")
+    print(presidente)
+    print("el candidato para alcalde es partido rojo")
+    print("el candidato para alcalde es partido azul")
+    print("el candidato para alcalde es partido verde")
+    print("el candidato para alcalde es partido morado")
+    alcalde=input("ingrese su voto: ")
+    print ("alcalde")
+else:
+    print("dpi incorrecto")
