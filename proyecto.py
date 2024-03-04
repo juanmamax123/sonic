018604c5a2747b91f59e2c81e2c5baed5da9d1d2
juanmamax123 <158304755+juanmamax123@users.noreply.github.com>
2024-03-04 11:32:31 -0600

    proyecto

---
+++ proyecto.py
@@ -0,0 +1,8 @@
+listapalabras=[]
+for i in range(10):
+    palabra=input("ingrese una palabra")
+    listapalabras.append(palabra)
+    print(listapalabras)
+    print("las palabras son" , palabra)
+
+
