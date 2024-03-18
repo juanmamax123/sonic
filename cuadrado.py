0c1cf51b5400a038980c75e805bf4e684de9adcc
juanmamax123 <158304755+juanmamax123@users.noreply.github.com>
2024-03-18 11:34:14 -0600

    librerias

---
+++ cuadrado.py
@@ -0,0 +1,44 @@
+
+
+import turtle
+t = turtle.Turtle()
+t.speed(1)
+t.penup()
+t.goto(-120, 100)
+t.pendown()
+for _ in range(8):
+    t.forward(45)
+    t.right(45)
+
+import turtle
+t = turtle.Turtle()
+t.speed(1)
+t.penup()
+t.goto(-50, 0)
+t.pendown()
+for _ in range(4):
+    t.forward(100)
+    t.right(90)
+
+import turtle
+t = turtle.Turtle()
+t.speed(1)
+t.penup()
+t.goto(80, 100)
+t.pendown()
+for _ in range(3):
+    t.forward(250)
+    t.right(120)    
+
+import turtle
+t = turtle.Turtle()
+t.speed(1)
+t.penup()
+t.goto(-70, 100)
+t.pendown()
+for _ in range(4):
+    t.forward(-200)
+    t.right(90)
+    t.forward(-100)
+    t.right(90)
+  
