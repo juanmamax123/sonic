3242f20d83cd141c9657d00a0b68a85a1efa09ff
juanmamax123 <158304755+juanmamax123@users.noreply.github.com>
2024-04-17 11:16:26 -0600

    Funciones 2

---
+++ trabajo2.py
@@ -0,0 +1,12 @@
+def nombre_de_la_funcion():
+    # codigo que realiza la tarea
+    pass
+
+def saludar( nombre):
+   print (f"¡hola,{nombre}! bienvenido a python")
+saludar ("Juan Ma") # imprime: ¡Hola, Ana! Bienvenida a Python.
+
+def sumar (a,b):
+    return a + b
+resultado = sumar (5, 3)
+print (resultado) # imprime: 8
