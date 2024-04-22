6f15e75e44effc7b2926c981932da8d78b2ee5db
juanmamax123 <158304755+juanmamax123@users.noreply.github.com>
2024-04-22 11:37:57 -0600

    funciones

---
+++ funciones.py
@@ -0,0 +1,24 @@
+
+def agregar(listaAgregar):
+    item=input ("ingrese un item")
+    listaAgregar.append(item)
+    return listaAgregar
+
+
+
+
+lista=[]
+print ("Bienvenido a nuestro menu, tiene 3 opciones. 1.agregar, 2. editar. 3. eliminar ")
+respuesta = input("ingrese una opcion ")
+if respuesta== "1":
+    lista=agregar(lista)
+elif respuesta==2
+    lista=editar 
+
+     item=input("Que producto desea eliminar")
+    if item=="fin":
+            lista.remove(item)
+elif eliminar=="no":
+
+
+
