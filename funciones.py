f08dc8192a3c8b6e1b95978fc53a0c3c4e51f9a0
juanmamax123 <158304755+juanmamax123@users.noreply.github.com>
2024-04-24 09:09:07 -0600

    funciones2

--- funciones.py
+++ funciones.py
@@ -1,24 +1,46 @@
-
-def agregar(listaAgregar):
-    item=input ("ingrese un item")
-    listaAgregar.append(item)
-    return listaAgregar
-
-
-
-
 lista=[]
-print ("Bienvenido a nuestro menu, tiene 3 opciones. 1.agregar, 2. editar. 3. eliminar ")
-respuesta = input("ingrese una opcion ")
-if respuesta== "1":
-    lista=agregar(lista)
-elif respuesta==2
-    lista=editar 
+def comenzar():
+    print("Bienvenido a tu lista de compras")
+    for _ in range(100):
+        item=input("ingrese un item: ")
+        if item=="fin":
+            print("Desea realizar algún cambio?")
+            break
+        else:
+            lista.append(item)
 
-     item=input("Que producto desea eliminar")
-    if item=="fin":
-            lista.remove(item)
-elif eliminar=="no":
+comenzar()
 
+def agregar():
+    item=input("Ingrese un item: ")
+    lista.append(item)
 
+def editar():
+    item=input("¿Qué item quiere editar?: ")
+    lista.append(item)
+
+def eliminar():
+    item=input("Que Item desea borrar?: ")
+    lista.remove(item)
+
+def finalizar():
+      item=input("Lista finalizada")
+      
+
+while True:
+    print("1. Ingresar un nuevo Item, 2. Editar Item, 3. Eliminar Item, 4. Finalizar lista")
+    opcion=input("Ingrese el número de la opción deseada: ")
+
+    if opcion == '1':
+            agregar()
+    elif opcion == '2':
+            editar()
+    elif opcion == '3':
+            eliminar()
+    elif opcion == "4":
+            finalizar()
+            break
+    else:
+            print("Opción no válida. Por favor, seleccione una opción válida.")
+print(lista)
 
