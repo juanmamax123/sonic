6b3b4663c83ab59214307b3284f7431398163fa8
juanmamax123 <158304755+juanmamax123@users.noreply.github.com>
2024-07-16 09:36:07 -0600

    historia

--- historia.py
+++ historia.py
@@ -22,10 +22,28 @@ imp = pygame.transform.scale(imp, (250, 250))
 cr7 = pygame.image.load("Diseño sin título.png")
 cr7 = pygame.transform.scale(cr7, (250, 250))
 
+shrek = pygame.image.load ("png-clipart-shrek-shrek-thumbnail-removebg-preview.png")
+shrek = pygame.transform.scale(shrek, (250, 250))
+
 estado = "inicio"
+
+primer= pygame.image.load("previews_0012_custom_1682997344.3685.jpg")
+primer = pygame.transform.scale(primer, (width, height)) 
+
 
 background_image = pygame.image.load("zzv4EXBoonre7Vmc7uweJWtnPJcHewebu3UVVhKPP9eQgcR0s0H_qr9GquXVU3PFdkXoirSLOSYkWjsfHNMqgA.webp")
 background_image = pygame.transform.scale(background_image, (width, height)) 
+
+
+texto1 = pygame.image.load ("Organizador Grafico Conceptual Doodle Multicolor.png")
+texto1 = pygame.transform.scale(texto1, (width, height))
+
+bienvenida= pygame.image.load ("shrekamor.png")
+bienvenida= pygame.transform.scale(bienvenida, (width, height))
+
+pantano = pygame.image.load("Captura de pantalla 2024-07-15 105556.png")
+pantano = pygame.transform.scale(pantano, (width, height)) 
+
 
 objetivo_size = 100
 objetivo_x = random.randint(square_size, width - objetivo_size - square_size)  # Evita que el objetivo esté en los márgenes iniciales del jugador
@@ -42,20 +60,35 @@ while running:
         if event.type == pygame.QUIT:
             running = False
         if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_1:
-                puntaje += 1
-
+            if event.key == pygame.K_RIGHT:
+                print (estado)
+                if estado=="inicio":
+                    estado="bienvenida"
+                elif estado== "bienvenida":
+                    estado ="texto1"
+                elif estado== "texto1":
+                   estado ="pantano"
+                     
 
 
     if estado == "inicio":
-        screen.blit(background_image,(0,0))
-        screen.blit(cr7, (175,170))
-        screen.blit(imp,(1300,170))
+        screen.blit(primer,(0,0))
+        puntaje_texto = font.render("El majestuoso Sherk y el rescate de Fiona ", True, (130,203,136,))
+        screen.blit(puntaje_texto, (550, 700))
         
+    if estado == "texto1":
+        screen.blit(texto1,(0,0))
 
+    
+    if estado == "bienvenida":
+        screen.blit(bienvenida,(0,0))
 
-    puntaje_texto = font.render("El majestuoso Sherk y el rescate de Fiona ", True, font_color)
-    screen.blit(puntaje_texto, (700, 500))
+    if estado == "pantano":
+        screen.blit(pantano,(0,0))
+        screen.blit(shrek, (400,550))
+    
+        
+    
 
     pygame.display.flip()
 
